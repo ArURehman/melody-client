0a1cda24ff38f7703ae1118a4dd711830727fb61
ArURehman <areebcodes1991@gmail.com>
2024-06-05 20:24:46 +0500

    Added sidebar and setup basic layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,16 @@
 import { Outlet } from "react-router-dom"
+import Sidebar from '../components/global/Sidebar'
 
 const Layout = () => {
   return (
-    <>
-        <Outlet />
-    </>
+    <div className="grid grid-flow-col grid-cols-6 h-screen overflow-auto">
+        <div className="col-span-1">
+            <Sidebar />
+        </div>
+        <div className="col-span-5">
+            <Outlet />
+        </div>
+    </div>
   )
 }
 
