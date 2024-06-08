de383c10d1aac32c6d43bae35efa97f2d4383db1
ArURehman <areebcodes1991@gmail.com>
2024-06-08 15:03:48 +0500

    Added library component for sidebar

---
+++ src/components/Sidebar/Library.jsx
@@ -0,0 +1,25 @@
+import { TbPlaylist } from "react-icons/tb"
+import { AiOutlinePlusCircle } from "react-icons/ai"
+
+const Library = () => {
+  const handleClick = () => {
+    // TODO: Handle upload
+  }
+
+  return (
+    <div className="flex flex-col">
+        <div className="flex justify-between items-center px-5 pb-2">
+            <div className="inline-flex items-center gap-x-2">
+                <TbPlaylist size={26} className="text-neutral-400"/>
+                <span className="text-md font-medium text-neutral-400">Your Library</span>
+            </div>
+            <AiOutlinePlusCircle size={20} className="text-neutral-400 cursor-pointer hover:text-white transition" onClick={handleClick}/>
+        </div>
+        <div className="flex flex-col gap-y-2 mt-4 px-3">
+            List of Songs
+        </div>
+    </div>
+  )
+}
+
+export default Library
