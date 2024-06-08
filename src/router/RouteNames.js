9cbed9f8b431399f18ce954530f656abba02b7b3
ArURehman <areebcodes1991@gmail.com>
2024-06-08 13:36:44 +0500

    Modified routes to contain more info

--- src/router/RouteNames.js
+++ src/router/RouteNames.js
@@ -1,5 +1,17 @@
+import { BiSearch } from "react-icons/bi";
+import { FaHome } from "react-icons/fa";
+
 const routeNames = {
-    HOME: '/'
+    HOME: {
+        icon: FaHome,
+        name: "Home",
+        path: "/",
+    },
+    SEARCH: {
+        icon: BiSearch,
+        name: "Search",
+        path: "/search",
+    },
 };
 
 export default routeNames;
