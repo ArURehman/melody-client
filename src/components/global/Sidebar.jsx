52deb93c2d23c927b46c8cf4f8a516a651078522
ArURehman <areebcodes1991@gmail.com>
2024-06-08 15:43:29 +0500

    Completed sidebar design

--- src/components/global/Sidebar.jsx
+++ src/components/global/Sidebar.jsx
@@ -1,6 +1,20 @@
+import Box from "./Box"
+import routeNames from '../../router/RouteNames'
+import SidebarItem from "../Sidebar/SidebarItem"
+import Library from "../Sidebar/Library"
+
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div className="w-[250px] h-full md:flex flex-col gap-y-2 p-2">
+        <Box styles="flex flex-col gap-y-4 px-5 py-4">
+          {Object.keys(routeNames).map((route) => (
+            <SidebarItem key={route.name} Icon={routeNames[route].icon} {...routeNames[route]}/>
+          ))}
+        </Box>
+        <Box styles="overflow-y-auto h-full py-4">
+          <Library />
+        </Box>
+    </div>
   )
 }
 
