46e3e4b425b81a48f81299318f69efb122f3b378
ArURehman <areebcodes1991@gmail.com>
2024-06-09 17:00:13 +0500

    Completed signup form

---
+++ src/components/forms/Signup.jsx
@@ -0,0 +1,84 @@
+import Input from "../global/Input"
+import { useState } from "react"
+import { auth } from "../../config/firebaseConfig";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import toast from "react-hot-toast";
+
+const Signup = ({ setShowLogin }) => {
+  
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignup = (e) => {
+    e.preventDefault();
+    if (username === '' || email === '' || password === '') {
+        toast.error(
+            'Please fill in all fields',
+            {
+                duration: 4000,
+                position: 'bottom-right',
+                className: 'bg-red-500 text-white font-sans font-semibold border border-red-600 rounded-md p-2 shadow-lg',
+            }
+        );
+        return;
+    }
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        const token = user.accessToken;
+        localStorage.setItem('token', token);
+        toast.success(
+            `Welcome ${username}!`,
+            {
+                duration: 4000,
+                position: 'bottom-right',
+                className: 'bg-gray-500 text-white font-sans font-semibold border border-gray-600 rounded-md p-2 shadow-lg',
+            }
+        );
+      })
+      .catch((error) => {
+        toast.error(
+            error.message.replace('Firebase: ', ''),
+            {
+                duration: 4000,
+                position: 'bottom-right',
+                className: 'bg-red-500 text-white font-sans font-semibold border border-red-600 rounded-md p-2 shadow-lg',
+            }
+        );
+      });
+  }
+
+  return (
+    <form>
+        <div className="mb-4">
+            <Input label="username" text="Username" type="text" placeholder="Username" state={username} setState={setUsername}/>
+        </div>
+        <div className="mb-4">
+            <Input label="email" text="Email" type="email" placeholder="Email" state={email} setState={setEmail}/>
+        </div>
+        <div className="mb-8">
+            <Input label="password" text="Password" type="password" placeholder="******************" state={password} setState={setPassword}/>
+        </div>
+        <div className="flex items-center justify-center">
+            <button
+            onClick={handleSignup}
+            className="bg-primary-500 hover:bg-primary-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full border border-primary-700 bg-neutral-700 hover:bg-neutral-800 focus:shadow-outline transition"
+            type="button"
+            >
+            Sign Up
+            </button>
+        </div>
+        <div className="text-center mt-4">
+            <button
+            className="inline-block align-baseline font-bold text-sm text-primary-500 hover:text-primary-800 hover:opacity-75 transition"
+            onClick={() => setShowLogin(true)}
+            >
+            {`Already have an account? Login`}
+            </button>
+        </div>
+    </form>
+  )
+}
+
+export default Signup
