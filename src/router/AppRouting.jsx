f3d9dcc517a4e4bf2ffc6c8e924b4d55c3e4b0e9
ArURehman <areebcodes1991@gmail.com>
2024-06-11 01:11:54 +0500

    Created search component and added it to routing

--- src/router/AppRouting.jsx
+++ src/router/AppRouting.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from '../components/Layout';
 import Home from '../pages/Home';
+import Search from '../pages/Search';
 import routes from './Routes';
 
 const AppRouting = () => {
@@ -9,6 +10,7 @@ const AppRouting = () => {
         <Routes>
             <Route element={<Layout />}> 
                 <Route path={routes.HOME} element={<Home />} /> 
+                <Route path={routes.SEARCH} element={<Search />} />
             </Route>
         </Routes>
     </BrowserRouter>
