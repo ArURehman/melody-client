cabc4ccfe9da12ff220784a6f3db57cebf23d37a
ArURehman <areebcodes1991@gmail.com>
2024-06-10 23:33:13 +0500

    Added way to display songs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,33 @@
 import ListItem from "../components/global/ListItem"
+import PageContent from "../components/global/PageContent";
+import { getSongs } from "../services/addSong";
+import { useState } from "react";
+
+export const revalidate = 0;
 
 const Home = () => {
+
+  const [songs, setSongs] = useState([])
+  getSongs().then(res => {
+    setSongs(res)
+  })
+
   return (
     <>
-    <div className="mb-2">
-      <h1 className="text-white text-3xl font-semibold">Welcome Back!</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
-        <ListItem image="../../../public/liked.jpg" name="Liked Songs" href='liked'/>
+      <div className="mb-2">
+        <h1 className="text-white text-3xl font-semibold">Welcome Back!</h1>
+        <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
+          <ListItem image="../../../public/liked.jpg" name="Liked Songs" href='liked'/>
+        </div>
       </div>
-    </div>
-    <div className="mt-4 mb-7">
-      <div className="flex justify-between items-center">
-        <h1 className="text-white text-2xl font-semibold">Newest Songs</h1>
+      <div className="mt-4 mb-7">
+        <div className="flex justify-between items-center">
+          <h1 className="text-white text-2xl font-semibold">Newest Songs</h1>
+        </div>
+        <div>
+          <PageContent songs={songs} />
+        </div>
       </div>
-      <div>
-        List of Songs
-      </div>
-    </div>
     </>
   )
 }
