973073efbf964a17d0f8ca307b8b4062fce6fa38
ArURehman <areebcodes1991@gmail.com>
2024-06-10 23:31:42 +0500

    Added get songs function

--- src/services/addSong.js
+++ src/services/addSong.js
@@ -1,4 +1,4 @@
-import { doc, setDoc, getDoc } from "firebase/firestore";
+import { doc, setDoc, getDoc, collection, query, orderBy, getDocs } from "firebase/firestore";
 import { db } from "../config/firebaseConfig";
 import { v4 as uuidv4 } from "uuid";
 
@@ -6,11 +6,13 @@ export const addSong = async (title, author, song, image, user) => {
     try {
         const songRef = doc(db, 'songs', uuidv4());
         await setDoc(songRef, {
+            id: songRef.id,
             title,
             author,
             song,
             image,
-            user
+            user,
+            created_at: new Date()
         });
         return songRef.id;
 
@@ -24,6 +26,21 @@ export const getSong = async (id) => {
         const songRef = doc(db, 'songs', id);
         const song = await getDoc(songRef);
         return song.data();
+    } catch (error) {
+        return Promise.reject('An error occured');
+    }
+}
+
+export const getSongs = async () => {
+    try {
+        const songs = [];
+        const songsRef = collection(db, 'songs');
+        const qry = query(songsRef, orderBy('created_at', 'desc'));
+        const songsSnapshot = await getDocs(qry);
+        songsSnapshot.forEach(song => {
+            songs.push(song.data());
+        });
+        return songs;
     } catch (error) {
         return Promise.reject('An error occured');
     }
